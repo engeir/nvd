dfc06de0e4ed6bb72c3ffddc6f7fe02c5e644876
engeir <engeir@pm.me>
2023-12-20 21:58:34 +0100

    chore: set run as default command

--- src/uninstall_command.sh
+++ src/uninstall_command.sh
@@ -4,7 +4,7 @@ if ! command -v gum >/dev/null 2>&1; then
     echo "gum is a required dependency of this scipt. See https://github.com/charmbracelet/gum."
 fi
 
-NVIM=$(find "$HOME"/.config/neovim_distros/ -maxdepth 1 -mindepth 1 -type d -printf '%f\n' | gum filter)
+NVIM=$(find "$HOME"/.config/neovim_distros/ -maxdepth 1 -mindepth 1 -type d | sed 's#.*/##' | gum filter)
 NVIM_DIR="$HOME"/.config/neovim_distros/"$NVIM"
 
 if [[ -d "$NVIM_DIR" ]]; then
@@ -12,15 +12,11 @@ if [[ -d "$NVIM_DIR" ]]; then
     #     --foreground 212 --border-foreground 212 --border double \
     #     --align center --width 50 --margin "1 2" --padding "2 4" \
     #     "Are you sure you want to delete the Neovim distro" "" "$NVIM" "" "($NVIM_DIR)"
-    echo "$(red_bold Are you sure you want to delete the Neovim distro "$NVIM"?)"
-    echo "$(magenta Located at "$NVIM_DIR")"
-    gum confirm || exit 0
+    red_bold Are you sure you want to delete the Neovim distro "$NVIM"?
+    magenta Located at "$NVIM_DIR"
+    gum confirm "Do you want to detele $NVIM?" || exit 0
     rm -rf "$NVIM_DIR"
 else
     echo "must be an existing directory"
     exit 1
 fi
-# if [[ $(find "$HOME/.config/neovim_distros/$NVIM" -maxdepth 1 -name "init.lua") == "" ]]; then
-#     NVIM=$(dirname "$(find "$HOME/.config/neovim_distros/$NVIM" -name "init.lua" | grep "nvim/init.lua")" | sed -n -e 's/^.*neovim_distros\///p')
-# fi
-# NVIM_APPNAME="neovim_distros/$NVIM" nvim "$@"
